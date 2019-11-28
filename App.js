57de499eb99a8c62b3713e6609821b2dddbefcc0
Fabian Fabro <fabrofabian@gmail.com>
2019-11-27 16:05:10 -0800

    First commit

---
+++ App.js
@@ -0,0 +1,200 @@
+import React from 'react';
+import { StyleSheet, TouchableOpacity, Text, View, Image } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { Audio } from 'expo-av';
+
+const audioBookPlaylist = [
+  {
+    title: 'Hamlet - Act I',
+    author: 'William Shakespeare',
+    source: 'Librivox',
+    uri:
+      'https://ia800204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act1_shakespeare.mp3',
+    imageSource:
+      'http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg'
+  },
+  {
+    title: 'Hamlet - Act II',
+    author: 'William Shakespeare',
+    source: 'Librivox',
+    uri:
+      'https://ia600204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act2_shakespeare.mp3',
+    imageSource:
+      'http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg'
+  },
+  {
+    title: 'Hamlet - Act III',
+    author: 'William Shakespeare',
+    source: 'Librivox',
+    uri:
+      'http://www.archive.org/download/hamlet_0911_librivox/hamlet_act3_shakespeare.mp3',
+    imageSource:
+      'http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg'
+  },
+  {
+    title: 'Hamlet - Act IV',
+    author: 'William Shakespeare',
+    source: 'Librivox',
+    uri:
+      'https://ia800204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act4_shakespeare.mp3',
+    imageSource:
+      'http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg'
+  },
+  {
+    title: 'Hamlet - Act V',
+    author: 'William Shakespeare',
+    source: 'Librivox',
+    uri:
+      'https://ia600204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act5_shakespeare.mp3',
+    imageSource:
+      'http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg'
+  }
+]
+
+class App extends React.Component {
+  state = {
+    isPlaying: false,
+    playbackInstance: null,
+    currentIndex: 0,
+    volume: 1.0,
+    isBuffering: false
+  }
+
+  async componentDidMount() {
+    try{
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: false,
+        interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+        playsInSilentModeIOS: true,
+        interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DUCK_OTHERS,
+        shouldDuckAndroid: true,
+        staysActiveInBackground: true,
+        playThroughEarpieceAndroid: true
+      })
+
+      this.loadAudio()
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async loadAudio() {
+    const {currentIndex, isPlaying, volume} = this.state;
+
+    try{
+      const playbackInstance = new Audio.Sound();
+      const source = {
+        uri: audioBookPlaylist[currentIndex].uri
+      }
+
+      const status = {
+        shouldPlay: isPlaying,
+        volume
+      }
+
+      playbackInstance.setOnPlaybackStatusUpdate(this.onPlaybackStatusUpdate);
+      await playbackInstance.loadAsync(source, status, false)
+      this.setState({playbackInstance})
+    } catch(e) {
+      console.log(e);
+    }
+    }
+
+    onPlaybackStatusUpdate = status => {
+      this.setState({
+        isBuffering: status.isBuffering
+      })
+    }
+  
+    handlePlayPause = async () => {
+      const { isPlaying, playbackInstance } = this.state;
+      isPlaying
+        ? await playbackInstance.pauseAsync()
+        : await playbackInstance.playAsync()
+
+      this.setState({
+        isPlaying: !isPlaying
+      })
+    }
+
+    handlePreviousTrack = async () => {
+      let { playbackInstance, currentIndex } = this.state;
+      if(playbackInstance){
+        await playbackInstance.unloadAsync()
+        currentIndex < audioBookPlaylist.length - 1
+          ? (currentIndex -= 1)
+          : (currentIndex = 0)
+        this.setState({
+          currentIndex
+        })
+        this.loadAudio()
+      }
+    }
+
+    handleNewTrack = async () => {
+      let { playbackInstance, currentIndex } = this.state;
+      if(playbackInstance) {
+        await playbackInstance.unloadAsync()
+        currentIndex < audioBookPlaylist.length - 1
+          ? (currentIndex +=1)
+          : (currentIndex = 0)
+        this.setState({
+          currentIndex
+        })
+        this.loadAudio()
+      }
+    }
+  
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Image
+          style={styles.albumCover}
+          source={{
+            uri: 'http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg'
+          }}
+        />
+        <View style={styles.controls}>
+          <TouchableOpacity style={styles.control} onPress={() => alert('')}>
+            <Ionicons name="ios-skip-backward" size={48} color="#444"/>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.control} onPress={() => alert('')}>
+            {this.state.isPlaying ? (
+              <Ionicons name="ios-pause" size={48} color="#444" />
+            ) : (
+              <Ionicons name="ios-play-circle" size={48} color="#444" />
+            )}
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.control} onPress={() => alert('')}>
+            <Ionicons name="ios-skip-forward" size={48} color="#444" />
+          </TouchableOpacity>
+        </View>
+        {/* <Text>Open up App.js to start working on your app!</Text> */}
+      </View>
+    );
+  }
+
+}
+
+export default App;
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  albumCover: {
+    width: 250,
+    height: 250
+  },
+  controls: {
+    flexDirection: 'row'
+  },
+  control: {
+    margin: 20
+  }
+});
